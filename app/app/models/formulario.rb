491184168a7ecc0c3762d5c0b8a023dcca13f4f9
DaviHVL <davihvl@gmail.com>
2025-12-08 00:01:43 -0300

    :sparkles: feat: implementação da exportação de resultados como .csv

--- app/app/models/formulario.rb
+++ app/app/models/formulario.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Formulario < ApplicationRecord
   has_many :questao_formularios, dependent: :destroy
 
@@ -5,4 +7,33 @@ class Formulario < ApplicationRecord
   
   has_many :formulario_turmas
   has_many :turmas, through: :formulario_turmas
+
+  def gerar_csv
+    CSV.generate(headers: true) do |csv|
+      questoes = questao_formularios.order(:id)
+      headers = ['Matrícula', 'Nome'] + questoes.map(&:texto_questao)
+      csv << headers
+
+      formulario_respondidos.includes(:usuario, questao_respondidas: :opcao_formulario).each do |resposta_geral|
+        linha = [
+          resposta_geral.usuario.matricula,
+          resposta_geral.usuario.nome
+        ]
+
+        questoes.each do |questao|
+          resposta = resposta_geral.questao_respondidas.find_by(questao_formulario: questao)
+          
+          valor = if resposta&.opcao_formulario
+                    resposta.opcao_formulario.texto_opcao 
+                  else
+                    resposta&.resposta 
+                  end
+          
+          linha << valor
+        end
+
+        csv << linha
+      end
+    end
+  end
 end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -5,25 +5,25 @@ Rails.application.routes.draw do
 
   get 'dashboard', to: 'dashboard#index'
 
-  # Rotas de Administração
   get 'admin', to: 'admins#dashboard', as: 'admin' 
   
-  # Rotas de Importação
   get 'admin/importar', to: 'admins#import_form', as: 'admin_importar_form'
   post 'admin/importar', to: 'admins#importar'
 
-  # Rotas de Envio de Formulários
   get 'admin/send_forms', to: 'admins#send_forms', as: 'admin_send_forms'
 
-  # Rotas de Gerenciamento de Templates
   get 'admin/templates', to: 'admins#edit_templates', as: 'admin_edit_templates'
   get 'admin/templates/new', to: 'admins#new_template', as: 'admin_new_template'
-  
-  # CORREÇÃO: Ativamos a rota POST. O helper gerado por essa linha é admin_templates_path,
-  # que o form_with espera para a criação (action: create).
   post 'admin/templates', to: 'admins#create_template', as: 'admin_templates' 
-
   delete 'admin/templates/:id', to: 'admins#destroy_template', as: 'admin_template_delete'
+
+  scope '/admin' do
+    resources :resultados, only: [:index] do
+      member do
+        get :baixar 
+      end
+    end
+  end
 
   root to: 'dashboard#index'
 
