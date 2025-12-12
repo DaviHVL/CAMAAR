b2909af7ae3def0c7cefa481589500f0fe99659f
DaviHVL <davihvl@gmail.com>
2025-12-12 16:44:34 -0300

    :recycle: refactor: refatoração de PasswordResetsController

--- app/app/models/usuario.rb
+++ app/app/models/usuario.rb
@@ -21,4 +21,19 @@ class Usuario < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+
+  def valid_reset_token?(token)
+    return false if reset_digest.nil? || password_reset_expired?
+    
+    BCrypt::Password.new(reset_digest).is_password?(token)
+  end
+
+  def clear_reset_digest!
+    update_columns(reset_digest: nil, reset_sent_at: nil)
+  end
+
+  def send_password_reset_email!
+    token = generate_password_reset_token!
+    UserMailer.password_reset(self, token).deliver_now
+  end
 end
